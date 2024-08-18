34ddcff17750bd9c3b28c29587d3ed69f2075d19
unakrejic <unakrejic@gmail.com>
2024-08-18 13:42:26 +0200

    2nd commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,70 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Button } from "./Button.js";
 
 function Navbar() {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerWidth <= 950) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton(); // Initial check when component mounts
+    window.addEventListener("resize", showButton);
+
+    // Cleanup the event listener when the component unmounts
+    return () => window.removeEventListener("resize", showButton);
+  }, []);
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
-          TRVL <i className="fab fa-typo3" />
+          Podcast Platform <i className="fab fa-typo3" />
         </Link>
         <div className="menu-icon" onClick={handleClick}>
           <i className={click ? "fas fa-times" : "fas fa-bars"} />
         </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/explore" className="nav-links" onClick={closeMobileMenu}>
+              Explore
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/fav" className="nav-links" onClick={closeMobileMenu}>
+              Favorites
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/sign-up"
+              className="nav-links-mobile"
+              onClick={closeMobileMenu}
+            >
+              Sign up
+            </Link>
+          </li>
+        </ul>
+        {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
       </div>
     </nav>
   );
 }
 
 export default Navbar;
-
-/*
-
-  TRVL <i className='fab fa-typo3' />
-
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/explore">Explore</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-        */
