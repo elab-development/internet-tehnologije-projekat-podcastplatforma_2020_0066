61ba97f5cb5ef6aa046e3c67006277b6e70ac656
ergaractamara <ergarac.t@gmail.com>
2024-08-22 15:35:23 +0200

    4th commit

---
+++ app/Models/Episode.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Episode extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'description', 'audio_url', 'podcast_id'];
+
+    public function podcast()
+    {
+        return $this->belongsTo(Podcast::class);
+    }
+}
