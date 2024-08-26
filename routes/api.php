5285169557e8b3103b3826e8a1e1d3cbbc657c23
ergaractamara <ergarac.t@gmail.com>
2024-08-26 20:46:13 +0200

    sesti

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PodcastController;
+use App\Http\Controllers\EpisodeController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -16,3 +17,10 @@ Route::post('/podcasts', [PodcastController::class, 'store'])->middleware('auth:
 Route::get('/podcasts/{id}', [PodcastController::class, 'show']);
 Route::put('/podcasts/{id}', [PodcastController::class, 'update'])->middleware('auth:sanctum'); 
 Route::delete('/podcasts/{id}', [PodcastController::class, 'destroy'])->middleware('auth:sanctum'); 
+
+Route::get('/podcasts/{podcastId}/episodes', [EpisodeController::class, 'index']);
+Route::post('/podcasts/{podcastId}/episodes', [EpisodeController::class, 'store'])->middleware('auth:sanctum');
+Route::get('/podcasts/{podcastId}/episodes/{id}', [EpisodeController::class, 'show']);
+Route::put('/podcasts/{podcastId}/episodes/{id}', [EpisodeController::class, 'update'])->middleware('auth:sanctum');
+Route::delete('/podcasts/{podcastId}/episodes/{id}', [EpisodeController::class, 'destroy'])->middleware('auth:sanctum');
+
