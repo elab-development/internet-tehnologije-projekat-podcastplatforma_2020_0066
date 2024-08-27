e2801672383c565f10cb48a77b6946b83c6d8085
ergaractamara <ergarac.t@gmail.com>
2024-08-28 00:03:05 +0200

    povezivanje react i laravel,.env,axios,cors

---
+++ novil/config/cors.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+
+    'paths' => ['*'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:3000',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
+
